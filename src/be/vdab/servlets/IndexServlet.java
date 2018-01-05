4be4c536cadd74f527f2e9229b5ce670093dd916
Olivier <Olivier@LOK111-03>
2018-01-05 09:02:46 +0100

    JSP+Servlet

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,10 +1,8 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,16 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("<html lang='nl'><head>");
-		out.println("<title>Frituur Frida</title></head>");
-		out.println("<body><h1> Vandaag zijn we ");
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		DayOfWeek vandaag = LocalDate.now().getDayOfWeek();
-		out.print(vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY ? "gesloten" : "open");
-		out.println("</h1></body></html>");
+		request.setAttribute("sluitingsdag", vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY ? "gesloten" : "open");
+		request.getRequestDispatcher(VIEW).forward(request, response);
+		
 	}
-
 }
